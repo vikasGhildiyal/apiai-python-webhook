a29f7b795c3c60dd5eef7a951b9b7c5b0b1b3667
vikasGhildiyal <41319365+vikasGhildiyal@users.noreply.github.com>
2018-08-23 13:13:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,14 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
+    #res = processRequest(req)
+    res={
+        "speech": "cold",
+        "displayText": "cold",
+        "data": {"slack": slack_message, "facebook": facebook_message},
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
 
     res = json.dumps(res, indent=4)
     # print(res)
